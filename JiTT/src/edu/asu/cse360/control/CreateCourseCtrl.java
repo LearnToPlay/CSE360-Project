365b597a7bf477988ac2948633cfb2d176d81efb
pursang <whagee@asu.edu>
2012-04-21 09:22:05 -0700

    another update3

---
+++ JiTT/src/edu/asu/cse360/control/CreateCourseCtrl.java
@@ -0,0 +1,12 @@
+package edu.asu.cse360.control;
+
+import edu.asu.cse360.model.Model;
+import edu.asu.cse360.view.View;
+
+public class CreateCourseCtrl extends Controller
+{
+	public CreateCourseCtrl (Model model, View view)
+	{
+		super(model, view);
+	}
+}
